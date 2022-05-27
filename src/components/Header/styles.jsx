d9bd6482e2e6deda800355a7963d08afb04fbeac
joaogcarlosf <joaogcarlosf@gmail.com>
2022-05-27 15:21:22 -0300

    mexendo nos styles

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  height: 60px;
-  background: ${(props) => props.theme.colors.primary};
+export const HeaderContainer = styled.div`
+  height: 7vh;
+  background: ${(props) => props.theme.colors.header};
   color: ${(props) => props.theme.colors.textLight};
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 30px;
+  border-bottom:  ${(props) => props.theme.colors.borderHeader};
 `;
