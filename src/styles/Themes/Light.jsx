68698a4782b9db9f6e189ed0e14155d72e011f24
joaogcarlosf <joaogcarlosf@gmail.com>
2022-05-27 15:21:58 -0300

    colocando mais elementos no thema light

--- src/styles/Themes/Light.jsx
+++ src/styles/Themes/Light.jsx
@@ -4,6 +4,8 @@ export default {
     colors: {
       primary: '#030027',
       secundary: '#F2F3D9',
+      header: 'radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)',
+      borderHeader: '1px solid #a4a4a49b',
   
       background: '#f5f5f5',
       text: '#333',
