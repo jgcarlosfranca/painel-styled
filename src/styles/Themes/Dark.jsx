dbea54a9533a347f7dc8764a01d2395ee0dff0cc
joaogcarlosf <joaogcarlosf@gmail.com>
2022-05-27 15:21:42 -0300

    colocando mais elementos no dark theme

--- src/styles/Themes/Dark.jsx
+++ src/styles/Themes/Dark.jsx
@@ -1,14 +1,15 @@
 export default {
-    title: 'dark-theme',
-  
-    colors: {
-      primary: '#030027',
-      secundary: '#F2F3D9',
-  
-      background: '#151E3F',
-      text: '#fff',
-      textLight: '#fff',
-      toggleThemeBackGrond: '#ffb300',
-      
-    },
-  };
+  title: "dark-theme",
+
+  colors: {
+    primary: "#030027",
+    secundary: "#F2F3D9",
+    header: 'radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)',
+    borderHeader: '1px solid #a4a4a49b',
+
+    background: "#151E3F",
+    text: "#fff",
+    textLight: "#fff",
+    toggleThemeBackGrond: "#ffb300",
+  },
+};
