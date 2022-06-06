a8bc5a78841cf342b84b9a8e2cb26a99096d4561
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-06 15:24:26 -0300

    ajuste de styles para o header

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
   height: 7vh;
+  width: 100%;
   background: ${(props) => props.theme.colors.header};
   color: ${(props) => props.theme.colors.textLight};
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 30px;
-  border-bottom:  ${(props) => props.theme.colors.borderHeader};
+  border-bottom: ${(props) => props.theme.colors.borderHeader};
 `;
