555799264547a42282931ba477e0b9dac2d13582
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-06 15:23:53 -0300

    criação de styles para o conteiner

---
+++ src/components/BodyConteiner/BodyConteiner.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledBodyConteiner = styled.div`
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+width: 80%;
+align-content: stretch;
+
+`;
