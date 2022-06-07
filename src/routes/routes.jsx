dc29b06ba6796ce99916c6f507177b33707dc697
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-07 17:08:23 -0300

    ajuste em routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -27,61 +27,61 @@ const routes = [
       name: "Analytics",
       icon: <BiAnalyse />,
     },
-    {
-      path: "/file-manager",
-      name: "File Manager",
-      icon: <AiTwotoneFileExclamation />,
-      subRoutes: [
-        {
-          path: "/settings/profile",
-          name: "Profile ",
-          icon: <FaUser />,
-        },
-        {
-          path: "/settings/2fa",
-          name: "2FA",
-          icon: <FaLock />,
-        },
-        {
-          path: "/settings/billing",
-          name: "Billing",
-          icon: <FaMoneyBill />,
-        },
-      ],
-    },
-    {
-      path: "/order",
-      name: "Order",
-      icon: <BsCartCheck />,
-    },
-    {
-      path: "/settings",
-      name: "Settings",
-      icon: <BiCog />,
-      exact: true,
-      subRoutes: [
-        {
-          path: "/settings/profile",
-          name: "Profile ",
-          icon: <FaUser />,
-        },
-        {
-          path: "/settings/2fa",
-          name: "2FA",
-          icon: <FaLock />,
-        },
-        {
-          path: "/settings/billing",
-          name: "Billing",
-          icon: <FaMoneyBill />,
-        },
-      ],
-    },
-    {
-      path: "/saved",
-      name: "Saved",
-      icon: <AiFillHeart />,
-    },
+    // {
+    //   path: "/file-manager",
+    //   name: "File Manager",
+    //   icon: <AiTwotoneFileExclamation />,
+    //   subRoutes: [
+    //     {
+    //       path: "/settings/profile",
+    //       name: "Profile ",
+    //       icon: <FaUser />,
+    //     },
+    //     {
+    //       path: "/settings/2fa",
+    //       name: "2FA",
+    //       icon: <FaLock />,
+    //     },
+    //     {
+    //       path: "/settings/billing",
+    //       name: "Billing",
+    //       icon: <FaMoneyBill />,
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/order",
+    //   name: "Order",
+    //   icon: <BsCartCheck />,
+    // },
+    // {
+    //   path: "/settings",
+    //   name: "Settings",
+    //   icon: <BiCog />,
+    //   exact: true,
+    //   subRoutes: [
+    //     {
+    //       path: "/settings/profile",
+    //       name: "Profile ",
+    //       icon: <FaUser />,
+    //     },
+    //     {
+    //       path: "/settings/2fa",
+    //       name: "2FA",
+    //       icon: <FaLock />,
+    //     },
+    //     {
+    //       path: "/settings/billing",
+    //       name: "Billing",
+    //       icon: <FaMoneyBill />,
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/saved",
+    //   name: "Saved",
+    //   icon: <AiFillHeart />,
+    // },
   ];
 
   
