d4dc0ca6b577dafd6d16e20c0b531095d5460754
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-07 17:07:57 -0300

    criacao do sidebar component

---
+++ src/components/SideBarTest/SideBarTeste.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  StyledSideBar,
+  StyledSideBarWrapper,
+  StyledSideBarMenu,
+  StyledSideBarList,
+  StyledSideBarItem,
+} from "./SideBarTeste.styles";
+
+import routes from "../../routes/routes";
+
+export const SideBarTeste = () => {
+  return (
+    <StyledSideBar>
+      <StyledSideBarWrapper>
+        <StyledSideBarMenu>
+          <StyledSideBarList>
+            {routes.map(() => {
+              <Link to={routes.path}>
+                <StyledSideBarItem>
+                  {routes.icon}
+                  {routes.name}
+                </StyledSideBarItem>
+              </Link>;
+            })}
+          </StyledSideBarList>
+        </StyledSideBarMenu>
+      </StyledSideBarWrapper>
+    </StyledSideBar>
+  );
+};
+
