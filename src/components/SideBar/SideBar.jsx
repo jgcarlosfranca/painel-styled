5f239777b0c1652d0d992d32abf251dfb0c15921
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-13 23:19:07 -0300

    ajuste

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  StyledSideBar,
+  StyledSideBarWrapper,
+  StyledSideBarMenu,
+  StyledSideBarList,
+  StyledSideBarItem,
+  StyledSideBarBodyList,
+} from "./SideBar.styles";
+
+import routes from "../../routes/routes";
+
+export const SideBarTeste = () => {
+  return (
+    <StyledSideBar>
+      <StyledSideBarWrapper>
+        <StyledSideBarMenu>
+          <StyledSideBarList>
+            Teste
+            {routes.map((route) => {
+              return (
+                <Link to={route.path}>
+                  <StyledSideBarItem>
+                    <StyledSideBarBodyList>
+                      {route.icon}
+                    </StyledSideBarBodyList>
+                    {route.name}
+                  </StyledSideBarItem>
+                </Link>
+              );
+            })}
+          </StyledSideBarList>
+        </StyledSideBarMenu>
+      </StyledSideBarWrapper>
+    </StyledSideBar>
+  );
+};
