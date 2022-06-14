922219e024b1e311cf77f1e3661e257fdb9c2105
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-13 23:19:14 -0300

    ajuste

---
+++ src/components/SideBar/SideBar.styles.jsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const StyledSideBar = styled.div`
+  height: 100vh;
+  position: relative;
+  background-color: ${(props) => props.theme.colors.sidebarBackgroud};
+  width: 15%;
+  min-width: 255px;
+`;
+
+//${(props) => props.theme.colors.sidebarBackgroud};
+export const StyledSideBarWrapper = styled.div`
+  padding: 20px;
+  color: ${(props) => props.theme.colors.text};
+`;
+
+export const StyledSideBarMenu = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const StyledSideBarList = styled.ul`
+  list-style: none;
+  padding: 5px;
+  font-size: 50px;
+`;
+
+export const StyledSideBarItem = styled.li`
+  padding: 15px 20px 20px 15px;
+  cursor: pointer;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  margin: 10px;
+  font-size: 20px !important;
+  color: ${(props) => props.theme.colors.text};
+  text-decoration: none;
+  &:hover {
+    color: ${(props) => props.theme.colors.text};
+    padding: 15px 30px 20px 25px;
+  }
+`;
+
+export const StyledSideBarBodyList = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px;
+`;
