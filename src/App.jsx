a74ea2671a075bc99c0352a2442a48998512c8c9
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-15 10:46:15 -0300

    compiled without warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
