e1bf6a414a4ae188a3620f877cb3fd4b4909d12b
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-16 22:49:54 -0300

    fix de warnings na routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,34 +1,28 @@
-import { FaBars, FaHome, FaLock, FaMoneyBill, FaUser } from "react-icons/fa";
+import { FaHome, FaUser } from "react-icons/fa";
 import { MdMessage } from "react-icons/md";
-import { BiAnalyse, BiSearch } from "react-icons/bi";
-import { BiCog } from "react-icons/bi";
-import { AiFillHeart, AiTwotoneFileExclamation } from "react-icons/ai";
-import { BsCartCheck } from "react-icons/bs"; 
-
+import { BiAnalyse } from "react-icons/bi";
 
 const routes = [
-    {
-      path: "/",
-      name: "Dashboard",
-      icon: <FaHome />,
-    },
-    {
-      path: "/users",
-      name: "Users",
-      icon: <FaUser />,
-    },
-    {
-      path: "/messages",
-      name: "Messages",
-      icon: <MdMessage />,
-    },
-    {
-      path: "/analytics",
-      name: "Analytics",
-      icon: <BiAnalyse />,
-    },
-    
-  ];
+  {
+    path: "/",
+    name: "Dashboard",
+    icon: <FaHome />,
+  },
+  {
+    path: "/users",
+    name: "Users",
+    icon: <FaUser />,
+  },
+  {
+    path: "/messages",
+    name: "Messages",
+    icon: <MdMessage />,
+  },
+  {
+    path: "/analytics",
+    name: "Analytics",
+    icon: <BiAnalyse />,
+  },
+];
 
-  
 export default routes;
