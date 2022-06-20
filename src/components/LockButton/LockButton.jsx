1dd3b1b1a53ec628a747817b05f12874d4e8681d
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-20 11:25:41 -0300

    voltando o toogleSidebar

--- src/components/LockButton/LockButton.jsx
+++ src/components/LockButton/LockButton.jsx
@@ -3,14 +3,13 @@ import useHover from "../../utils/Hooks/useHover";
 import { StyledLookButton } from "./LockButton.styles";
 import { TiLockClosed, TiLockOpen } from "react-icons/ti";
 
-export const LockButton = () => {
+export const LockButton = (props) => {
   const [hover, isHover] = useHover();
 
-  const icon = isHover ? <TiLockClosed /> : <TiLockOpen />; 
+  const icon = isHover ? <TiLockClosed /> : <TiLockOpen />;
 
   return (
-    <StyledLookButton ref={hover}
-    >
+    <StyledLookButton ref={hover} onClick={props.toogleSidebar}>
       {icon}
     </StyledLookButton>
   );
