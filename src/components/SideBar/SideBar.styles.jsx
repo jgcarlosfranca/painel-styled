74fe40fded42342aa10ed72eaaa578ddec8ec871
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-21 14:15:24 -0300

    stylização do scroll da sidebar

--- src/components/SideBar/SideBar.styles.jsx
+++ src/components/SideBar/SideBar.styles.jsx
@@ -6,7 +6,21 @@ export const StyledSideBar = styled.div`
   background-color: ${(props) => props.theme.colors.sidebarBackgroud};
   width: 15%;
   min-width: 255px;
-  a{
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.colors.sidebarBackgroud};
+  }
+
+  ::-webkit-scrollbar-thumb {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: ${(props) => props.theme.colors.sideBarThumb}; 
+    border-radius: 100px;
+  }
+  a {
     text-decoration: none;
   }
 `;
@@ -17,7 +31,6 @@ export const StyledSideBarHeader = styled.div`
   flex-direction: row;
   justify-content: space-between;
   cursor: default;
-  
 `;
 
 export const StyledSideBarWrapper = styled.div`
