5cdbe23e7f6fbb1a976d779ef15fdf9bd2ea29ba
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-23 16:36:26 -0300

    mudança de cor primaria em tema Light

--- src/styles/Themes/Light.jsx
+++ src/styles/Themes/Light.jsx
@@ -2,7 +2,7 @@ export default {
     title: 'light-theme',
   
     colors: {
-      primary: '#030027',
+      primary: '#333',
       secundary: '#F2F3D9',
       header: 'radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)',
       borderHeader: '1px solid #a4a4a49b',
