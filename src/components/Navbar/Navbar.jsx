b0d376ab785331ce2462ee51cbd15d1c5e746c3f
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-24 23:51:31 -0300

    excluindo nav sem styles

--- src/components/Navbar/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledNavbar } from "./Navbar.styles";
-
-export const Navbar = (props) => {
-  return <StyledNavbar>
-    {props.children}
-  </StyledNavbar>;
-};
