74cc538717cafa354e302dfbf2cd3f34d94c0c04
joaogcarlosf <joaogcarlosf@gmail.com>
2022-06-29 15:49:58 -0300

    criação do hover para Dark Theme

--- src/styles/Themes/Dark.jsx
+++ src/styles/Themes/Dark.jsx
@@ -4,15 +4,15 @@ export default {
   colors: {
     primary: "#283046",
     secundary: "#F2F3D9",
-    header: 'radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)',
-    borderHeader: '1px solid #a4a4a49b',
+    header: "radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)",
+    borderHeader: "1px solid #a4a4a49b",
 
     background: "#111829",
+    hoverLockButton: "#7e7e7ed3",
     text: "#fff",
     textLight: "#fff",
     toggleThemeBackGrond: "#ffb300",
-    sidebarBackgroud: '#283046',
-    sideBarThumb: '#a4a4a46a',
-    
+    sidebarBackgroud: "#283046",
+    sideBarThumb: "#a4a4a46a",
   },
 };
