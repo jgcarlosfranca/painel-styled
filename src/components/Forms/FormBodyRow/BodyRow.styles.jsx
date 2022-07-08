888ff6bfd0c9d24dfe67af9a2f766fa5286acef1
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-07 21:55:40 -0300

    body row styles

---
+++ src/components/Forms/FormBodyRow/BodyRow.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledFormBodyRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+`;
