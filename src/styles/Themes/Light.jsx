fd48937a9418e719295debfb1caec51523e11388
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-12 15:47:38 -0300

    fix de cor de fundo do thema light

--- src/styles/Themes/Light.jsx
+++ src/styles/Themes/Light.jsx
@@ -2,7 +2,7 @@ export default {
     title: 'light-theme',
   
     colors: {
-      primary: '#333',
+      primary: '#202020',
       secundary: '#F2F3D9',
       header: 'radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)',
       borderHeader: '1px solid #a4a4a49b',
@@ -13,6 +13,6 @@ export default {
       textLight: '#f5f5f5',
       toggleThemeBackGrond: '#f5f5f5',
       sidebarBackgroud: '#FFF',
-      sideBarThumb: '#a4a4a46a',
+      sideBarThumb: '#a4a4a496',
     },
   };
