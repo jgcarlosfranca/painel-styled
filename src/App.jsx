994718c2e36b1262dd7c53e5e011df18284fd36e
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-15 15:51:06 -0300

    arrumando sidebar geral

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import GlobalStyles from "./styles/global";
 import Header from "./components/Header/Header";
-import { SideBar } from "./components/SideBar";
+import { SideBar } from "./components/SideBar/LargeSideBar";
 import { BodyConteiner } from "./components/BodyConteiner";
 
 import usePersistedState from "./utils/Hooks/usePersistentState";
