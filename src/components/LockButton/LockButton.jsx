fc4d477a216d6358741ded6015b7a305eb5e4935
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-15 15:52:15 -0300

    fix lockbutton

--- src/components/LockButton/LockButton.jsx
+++ src/components/LockButton/LockButton.jsx
@@ -4,13 +4,10 @@ import { StyledLookButton } from "./LockButton.styles";
 import { TiLockClosed, TiLockOpen } from "react-icons/ti";
 
 export const LockButton = (props) => {
-  // const [hover, isHover] = useHover();
   const [isHover, setIsHover] = useState(false);
 
   const icon = isHover ? <TiLockOpen /> : <TiLockClosed />;
-  /*<StyledLookButton ref={hover} onClick={props.toogleSidebar}>
-      {icon}
-    </StyledLookButton>*/
+ 
   return (
     <StyledLookButton
       onMouseEnter={() => {
@@ -19,7 +16,6 @@ export const LockButton = (props) => {
       onMouseLeave={() => {
         setIsHover(false);
       }}
-      
     >
       {icon}
     </StyledLookButton>
