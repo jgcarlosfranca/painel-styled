b67b5a8ecf90efa849b391d70824b29f55d6f994
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-20 11:21:35 -0300

    criando o sidebar geral

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,59 +1,15 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import {
-  StyledSideBar,
-  StyledSideBarWrapper,
-  StyledSideBarMenu,
-  StyledSideBarList,
-  StyledSideBarItem,
-  StyledSideBarBodyList,
-  StyledSideBarLinkName,
-  StyledSideBarTextConteiner,
-  StyledSideBarHeader,
-} from "./SideBar.styles";
-import { LockButton } from "../../LockButton";
-
-import routes from "../../routes/routes";
+import { LargeSideBar } from "./LargeSideBar";
+import { SlimSideBar } from "./SlimSidebar";
+import { useSelector } from "react-redux/es/exports";
 
 export const SideBar = () => {
-  const [sideBarWidth, setSideBarWidth] = useState("15%");
-  const [sideBarCollapse, setSideBarCollapse] = useState(false);
-
-  const toogleSidebar = () => {
-    console.log("clicou na lock sidebar!");
-    if (sideBarCollapse) {
-      setSideBarWidth("5%");
-      setSideBarCollapse(false);
-    } else {
-      setSideBarWidth("15%");
-      setSideBarCollapse(true);
-    }
-  };
-
-  return (
-    <StyledSideBar>
-      <StyledSideBarWrapper>
-        <StyledSideBarMenu>
-          <StyledSideBarList>
-            <StyledSideBarHeader>
-              Teste <LockButton toogleSidebar={toogleSidebar} />
-            </StyledSideBarHeader>
-            {routes.map((route) => {
-              return (
-                <Link to={route.path}>
-                  <StyledSideBarItem>
-                    <StyledSideBarBodyList>{route.icon}</StyledSideBarBodyList>
-                    <StyledSideBarLinkName>{route.name}</StyledSideBarLinkName>
-                  </StyledSideBarItem>
-                </Link>
-              );
-            })}
-            <StyledSideBarTextConteiner>
-              another test
-            </StyledSideBarTextConteiner>
-          </StyledSideBarList>
-        </StyledSideBarMenu>
-      </StyledSideBarWrapper>
-    </StyledSideBar>
+  const sideBarState = useSelector(
+    (state) => state.sidebarBehavior.sidebarWidth
   );
+
+  const componentRender =
+    sideBarState === "SIDEBAR_LARGE" ? <LargeSideBar /> : <SlimSideBar />;
+
+  return <>{componentRender}</>;
 };
