2a89af30f878dadafe51196dff659ae25228fe0c
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-22 14:56:17 -0300

    criacao component tools

---
+++ src/pages/Tools/Tools.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyledToolsConteiner } from "./Tools.styles";
+import Tabs from "../../components/Tab/Tab";
+
+const Tools = () => {
+  return (
+    <StyledToolsConteiner>
+      Toooooooools
+      <Tabs />
+    </StyledToolsConteiner>
+  );
+};
+
+export default Tools;
