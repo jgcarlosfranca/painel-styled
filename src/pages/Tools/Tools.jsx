caa84b92f35c625c35427deef604d28ef3ec0695
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-25 13:49:21 -0300

    criando logica para tab

--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { StyledToolsConteiner } from "./Tools.styles";
 import Tabs from "../../components/Tab/Tab";
+const numTabs = [
+  { titulo: "tab1", body: "bodyTab 1" },
+  { titulo: "tab2", body: "bodyTab 2" },
+  { titulo: "tab3", body: "bodyTab 3" },
+  { titulo: "tab4", body: "bodyTab 4" },
+  { titulo: "tab5", body: "bodyTab 5" },
+];
 
 const Tools = () => {
   return (
     <StyledToolsConteiner>
       Toooooooools
-      <Tabs />
+      <Tabs tabs={numTabs}/>
     </StyledToolsConteiner>
   );
 };
