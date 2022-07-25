cea79011b47f50dd9994bd2b1baf48a4f7c73970
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-25 13:08:21 -0300

    criando component tab

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { StyledTab, StyledTabUl } from "./Tab.styles";
 
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState("tab1");
+  const handleTab = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <StyledTab>
       {/* Tab nav */}
       <StyledTabUl>
-        <li>Tab 1</li>
-        <li>Tab 2</li>
+        <li
+          className={activeTab === "tab1" ? "active" : ""}
+          onClick={() => handleTab("tab1")}
+        >
+          Tab 1
+        </li>
+
+        <li
+          className={activeTab === "tab2" ? "active" : ""}
+          onClick={() => handleTab("tab2")}
+        >
+          Tab 2
+        </li>
+        <li
+          className={activeTab === "tab3" ? "active" : ""}
+          onClick={() => handleTab("tab3")}
+        >
+          Tab 3
+        </li>
+        <li
+          className={activeTab === "tab4" ? "active" : ""}
+          onClick={() => handleTab("tab4")}
+        >
+          Tab 4
+        </li>
       </StyledTabUl>
       <div className="outlet">{/* content will be shown here */}</div>
     </StyledTab>
