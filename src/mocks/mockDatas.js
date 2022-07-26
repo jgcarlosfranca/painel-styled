f4ba29806fec2a3b398844ae3fc90e80c6c8510e
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-26 14:49:34 -0300

    criação de mocks para exemplo de data nos components

---
+++ src/mocks/mockDatas.js
@@ -0,0 +1,10 @@
+const numTabs = [
+    { titulo: "tab1", body: "bodyTab 1" },
+    { titulo: "tab2", body: "bodyTab 2" },
+    { titulo: "tab3", body: "bodyTab 3" },
+    { titulo: "tab4", body: "bodyTab 4" },
+    { titulo: "tab5", body: "bodyTab 5" },
+];
+
+
+export default numTabs;
