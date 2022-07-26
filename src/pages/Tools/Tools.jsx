ff5c26ab968a314b30df37266a9ab93606e99468
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-26 14:49:45 -0300

    tirando mocks do component

--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import { StyledToolsConteiner } from "./Tools.styles";
 import Tabs from "../../components/Tab/Tab";
-const numTabs = [
-  { titulo: "tab1", body: "bodyTab 1" },
-  { titulo: "tab2", body: "bodyTab 2" },
-  { titulo: "tab3", body: "bodyTab 3" },
-  { titulo: "tab4", body: "bodyTab 4" },
-  { titulo: "tab5", body: "bodyTab 5" },
-];
+import numTabs from "../../mocks/mockDatas";
 
 const Tools = () => {
   return (
     <StyledToolsConteiner>
-      Toooooooools
-      <Tabs tabs={numTabs}/>
+      <Tabs tabs={numTabs} />
     </StyledToolsConteiner>
   );
 };
