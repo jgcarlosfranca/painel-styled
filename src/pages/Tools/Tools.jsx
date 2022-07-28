7bf4add6e4e421656a9d065f62ff4a9ecfe7d093
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-28 15:16:46 -0300

    colocando components na pagina de tools

--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { StyledToolsConteiner } from "./Tools.styles";
 import Tabs from "../../components/Tab/Tab";
-import numTabs from "../../mocks/mockDatas";
+import mocks from "../../mocks/mockDatas";
+import { Accordion } from "../../components/Accordion";
+import { CardButton } from "../../components/CardButton";
+import { Card } from "../../components/Card";
+import { Button } from "../../components/Button";
+import imagemTeste from "../../assets/img/elemento_agua.png"
 
 const Tools = () => {
   return (
     <StyledToolsConteiner>
-      <Tabs tabs={numTabs} />
+      <Tabs tabs={mocks.numTabs} />
+      <Accordion
+        title={mocks.accordionData.question}
+        content={mocks.accordionData.answer}
+      />
+
+      <Accordion
+        title={mocks.accordionData.question}
+        content={mocks.accordionData.answer}
+      />
+      <Accordion
+        title={mocks.accordionData.question}
+        content={mocks.accordionData.answer}
+      />
+      <Card>
+        <CardButton image={imagemTeste} onClick={() => console.log("clicou")} />
+        <Button />
+      </Card>
     </StyledToolsConteiner>
   );
 };
