befb01e499a9fa27abca5a17b3623347637ddc48
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-28 15:14:13 -0300

    criando box shadow

--- src/components/Button/Button.styles.jsx
+++ src/components/Button/Button.styles.jsx
@@ -1,22 +1,33 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.button`
+  width: ${(props) => props.tamanho};
   font-size: 1rem;
   font-weight: 600;
   text-transform: uppercase;
   padding: 0.6em;
-  border: none;
-  border-radius: var(--borda-radius);
-  box-shadow: var(--sombra-padrao);
-  color: ${(props) => (props.corBotao ? "var(--cor-branco)" : "#000")};
-  background-color: ${(props) => props.corBotao};
-  opacity: 1;
+  color: ${(props) => props.theme.colors.text};
+  background-color: ${(props) => props.theme.colors.primaryButtonColor};
+  border-radius: 5px;
   cursor: pointer;
-  transition: opacity 180ms ease-in-out;
+  transition: transform ease-in 0.5s;
 
   &:hover {
-    transform: scale(1.1);
-    transition: all 0.25s;
-    color: ${(props) => (props.corBotaoHover ? props.corBotaoHover : "#000")};
+    color: ${(props) => props.theme.colors.text};
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+      background 0s, border 0s;
+    box-shadow: 0px 0px 10px 3px
+      ${(props) => props.theme.colors.primaryButtonColor};
+    -webkit-box-shadow: 0px 0px 10px 3px
+      ${(props) => props.theme.colors.primaryButtonColor};
+    -moz-box-shadow: 0px 0px 10px 3px
+      ${(props) => props.theme.colors.primaryButtonColor};
+  }
+
+  &:active {
+    transition: transform ease-in 0.1s;
+    transform: scale(0.9);
+    box-shadow: 0 2px 25px ${(props) => props.theme.colors.primaryButtonColor};
   }
 `;
