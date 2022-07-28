3975e9593dde8b6f201fecd725741cfaa7865571
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-28 15:14:39 -0300

    criando styles para cards

--- src/components/Card/Card.styles.jsx
+++ src/components/Card/Card.styles.jsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
-export const StyledButton = styled.div`
+export const StyledCard = styled.div`
   display: flex;
   flex-direction: row;
+  background: ${(props) => props.theme.colors.sidebarBackgroud};
+  margin: 1.5rem 1.5rem 0.5rem 1.5rem;
+  padding: 1.2rem 1rem;
+  color: ${(props) => props.theme.colors.text};
+  border-radius: 1rem;
+
 `;
