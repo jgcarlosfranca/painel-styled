e94242bd922e3053d985272e177204e1f4fb8935
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-28 15:17:06 -0300

    criando novos styles

--- src/pages/Tools/Tools.styles.jsx
+++ src/pages/Tools/Tools.styles.jsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 export const StyledToolsConteiner = styled.div`
-  width: 100%;
+  width: 93%;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
-  padding: 1vh 5px 0 5px;
+  padding: 1vh 5px 1vh 5px;
+  margin: 1.5rem 1.5rem 0.5rem 1.5rem;
+  box-sizing: border-box;
+  @media (max-width: 768px) {
+    width: 90%;
+  }
 `;
