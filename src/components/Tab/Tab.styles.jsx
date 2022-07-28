5e54e9d73a8c1afe2d8ac2d1a45314076cac4de0
joaogcarlosf <joaogcarlosf@gmail.com>
2022-07-28 15:16:25 -0300

    fix color tab

--- src/components/Tab/Tab.styles.jsx
+++ src/components/Tab/Tab.styles.jsx
@@ -7,7 +7,7 @@ export const StyledTab = styled.div`
   background: ${(props) => props.theme.colors.sidebarBackgroud};
   margin: 1.5rem 1.5rem 0.5rem 1.5rem;
   padding: 1.2rem 1rem;
-  color: #e8f0f2;
+  color: ${(props) => props.theme.colors.text};
   border-radius: 1rem;
 
   @media (max-width: 769px) {
