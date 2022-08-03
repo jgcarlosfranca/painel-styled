001ed31fba29c105e1c7f174602e43d91c36ae71
joaogcarlosf <joaogcarlosf@gmail.com>
2022-08-02 22:22:49 -0300

    start criação da modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  StyledModel,
+  StyledContainerModel,
+  StyledHeaderModal,
+  StyledHBarModal,
+  StyledSlotModal,
+  StyledButtonBarModal,
+  StyledButtonModal,
+} from "./Modal.styles";
+
+export default function ConfirmationModal(props) {
+
+}
