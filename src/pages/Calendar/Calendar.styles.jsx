99a156ef6cc2485424c7f5442a3c674efad90413
joaogcarlosf <joaogcarlosf@gmail.com>
2022-08-29 18:44:00 -0300

    criação e estrutura pagina de calendar styles

---
+++ src/pages/Calendar/Calendar.styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledCalendarConteiner = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 1vh 5px 0 5px;
+`;
