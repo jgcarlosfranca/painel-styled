461e61cd892a18585155b9f2f024be6f6cb254b3
joaogcarlosf <joaogcarlosf@gmail.com>
2022-08-31 23:03:22 -0300

    criação da pagina Reports styles

---
+++ src/pages/Reports/Reports.styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledReportsConteiner = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 1vh 5px 0 5px;
+`;
