1ee1029beebb060fdb15ff7edb9ac7628c0314eb
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-12 10:22:05 -0300

    criação da table styles

---
+++ src/components/Table/Table.styles.jsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+export const StyledTab = styled.div`
+  width: 100%;
+  height: auto;
+  min-height: 400px;
+  background: ${(props) => props.theme.colors.sidebarBackgroud};
+  margin: 1.5rem 1.5rem 0.5rem 1.5rem;
+  padding: 1.2rem 1rem;
+  color: ${(props) => props.theme.colors.text};
+  border-radius: 1rem;
+
+  @media (max-width: 769px) {
+    padding: 2rem 0;
+  }
+`;
+
+export const StyledTabUl = styled.ul`
+  width: 60%;
+  margin: 0 auto 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+
+  li {
+    width: 50%;
+    padding: 0.9rem;
+    list-style: none;
+    text-align: center;
+    cursor: pointer;
+    transition: all 0.7s;
+  }
+
+  li:nth-child(n) {
+    margin: 5px;
+    border-radius: 5px;
+    font-size: 20px;
+    font-family: Bahnschrift SemiBold;
+  }
+
+  li:hover {
+    background-color: ${(props) => props.theme.colors.primaryButtonColor};
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+      background 0s, border 0s;
+    box-shadow: 0px 0px 10px 3px
+      ${(props) => props.theme.colors.primaryButtonColor};
+    -webkit-box-shadow: 0px 0px 10px 3px
+      ${(props) => props.theme.colors.primaryButtonColor};
+    -moz-box-shadow: 0px 0px 10px 3px
+      ${(props) => props.theme.colors.primaryButtonColor};
+  }
+
+  li.active {
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+      background 0s, border 0s;
+    background-color: ${(props) => props.theme.colors.primaryButtonColor};
+  }
+`;
+
+export const StyledTabBody = styled.div`
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, background 0s,
+    border 0s;
+  width: 100%;
+  height: auto;
+  min-height: 400px;
+  color: ${(props) => props.theme.colors.primaryButtonColor};
+`;
