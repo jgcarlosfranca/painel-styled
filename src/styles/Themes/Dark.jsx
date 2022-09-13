b317e6fe0d3872f42fd62a003f2978e31053e97a
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-13 13:23:15 -0300

    fix Dark theme

--- src/styles/Themes/Dark.jsx
+++ src/styles/Themes/Dark.jsx
@@ -1,8 +1,10 @@
-export default {
+import { DefaultTheme } from "styled-components";
+
+const darkTheme = {
   title: "dark-theme",
 
   colors: {
-    primary: "#283046",
+    primary: "#202020",
     secundary: "#F2F3D9",
     header: "radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)",
     borderHeader: "1px solid #a4a4a49b",
@@ -13,8 +15,19 @@ export default {
     textLight: "#f5f5f5",
     toggleThemeBackGrond: "#ffb300",
     sidebarBackgroud: "#283046",
+    contrastBackgroud: "#343D55",
     sideBarThumb: "#a4a4a46a",
     primaryButtonColor: "#7367f0",
-    glassBackGround: "#28304626"
+    glassBackGround: "#28304626",
+    borderAccordion: "1px solid #e4e4e49b",
+    checkBoxSecundary: "#bebebe",
+    loadingSpiner: "#F2F3D9",
+    tables: {
+      borderTd: "2px solid #dee2e6",
+      borderTr: "2px solid #dee2e6",
+      updateIcons: "#f5f5f5",
+    },
   },
 };
+
+export default darkTheme;
