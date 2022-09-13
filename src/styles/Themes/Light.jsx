22e5ab6bc84622b4199ca8c4da74551b6640a18d
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-13 13:23:27 -0300

    fix Light Theme

--- src/styles/Themes/Light.jsx
+++ src/styles/Themes/Light.jsx
@@ -1,20 +1,33 @@
-export default {
-    title: 'light-theme',
-  
-    colors: {
-      primary: '#202020',
-      secundary: '#F2F3D9',
-      header: 'radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)',
-      borderHeader: '1px solid #a4a4a49b',
-  
-      background: '#F2F2F2', 
-      hoverLockButton: "#a4a4a4d4",
-      text: '#333',
-      textLight: '#f5f5f5',
-      toggleThemeBackGrond: '#f5f5f5',
-      sidebarBackgroud: '#FFF',
-      sideBarThumb: '#a4a4a496',
-      primaryButtonColor: "#7367f0",
-      glassBackGround: '#ffffff17',
+import { DefaultTheme } from "styled-components";
+
+const LightTheme = {
+  title: "light-theme",
+
+  colors: {
+    primary: "#202020",
+    secundary: "#F2F3D9",
+    header: "radial-gradient(circle, rgba(2,11,108,1) 0%, rgba(3,0,39,1) 100%)",
+    borderHeader: "1px solid #a4a4a49b",
+
+    background: "#F2F2F2",
+    hoverLockButton: "#a4a4a4d4",
+    text: "#333",
+    textLight: "#f5f5f5",
+    toggleThemeBackGrond: "#f5f5f5",
+    sidebarBackgroud: "#FFF",
+    contrastBackgroud: "#F3F2F7",
+    sideBarThumb: "#a4a4a496",
+    primaryButtonColor: "#7367f0",
+    glassBackGround: "#ffffff17",
+    borderAccordion: "1px solid #2424249b",
+    checkBoxSecundary: "#bebebe",
+    loadingSpiner: "#0066FF",
+    tables: {
+      borderTd: "2px solid #a4a4a49b",
+      borderTr: "2px solid #a4a4a49b",
+      updateIcons: "#000000",
     },
-  };
+  },
+};
+
+export default LightTheme;
