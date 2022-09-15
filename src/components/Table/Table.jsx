c07062f30e62e3a1dbd7080eecf5785a1c0446ec
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-15 10:28:53 -0300

    criação da table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,54 +1,32 @@
-import React, { useState } from "react";
-import { StyledTab, StyledTabUl, StyledTabBody } from "./Table.styles";
+import React from "react";
+import { StyledTableResponsiveConteiner, StyledTable } from "./Table.styles";
 
 const Table = (props) => {
-  const deafultActiveTab = props.tabs[0].titulo
-  const [activeTab, setActiveTab] = useState(deafultActiveTab);
-  
-  const handleTab = (tab) => {
-    setActiveTab(tab);
-  };
-
-  const tabBody = () => {
-    let component = <div></div>;
-    switch (activeTab) {
-      case "tab1":
-        component = <div> body da Tab1</div>;
-        break;
-      case "tab2":
-        component = <div> body da Tab2</div>;
-        break;
-      case "tab3":
-        component = <div> body da Tab3</div>;
-        break;
-      case "tab4":
-        component = <div> body da Tab4</div>;
-        break;
-
-      default:
-        component = <div> body da Tab1</div>;
-        break;
-    }
-    return component;
-  };
-
+  const { titles, body, checkAll, setCheckAll } = props;
   return (
-    <StyledTab>
-      {/* Tab nav */}
-      <StyledTabUl>
-        {props.tabs.map((tab) => {
-          return (
-            <li
-              className={activeTab === tab.titulo ? "active" : ""}
-              onClick={() => handleTab(tab.titulo)}
-            >
-              {tab.titulo}
-            </li>
-          );
-        })}
-      </StyledTabUl>
-      <StyledTabBody>{tabBody()}</StyledTabBody>
-    </StyledTab>
+    <StyledTableResponsiveConteiner>
+      <StyledTable>
+        <thead>
+          <tr>
+            {titles.map((title, index) => {
+              return index === 0 ? (
+                <th key="checkBoxSelect">
+                  <input
+                    type="checkbox"
+                    name="checkBoxSelect"
+                    checked={checkAll}
+                    onChange={setCheckAll}
+                  />
+                </th>
+              ) : (
+                <th key={title}>{title}</th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>{body}</tbody>
+      </StyledTable>
+    </StyledTableResponsiveConteiner>
   );
 };
 export default Table;
