4945363e12e0868d5faf1c888a39ea3822bed4bc
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-16 08:02:13 -0300

    colocando função reducer no store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sideBarBehaviorReducer from "../features/sideBarBehaviorSlice";
+import postsReducer from "../features/posts/postsSlice";
 
 
 export const store = configureStore({
     reducer: {
         sidebarBehavior: sideBarBehaviorReducer,
+        posts: postsReducer,
     }
 })
