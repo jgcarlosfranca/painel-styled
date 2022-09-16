9dc644b95d20b6d96a488b7d3990e1d8372d33d8
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-16 08:02:27 -0300

    component de post page redux

---
+++ src/redux/features/posts/PostsList.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
+
+import React from "react";
+
+const PostsList = () => {
+  const posts = useSelector(selectAllPosts);
+
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+
+export default PostsList;
