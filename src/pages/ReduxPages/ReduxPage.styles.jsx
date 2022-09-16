894ab691031a1af6b3ddadba9b5d9349df5274ed
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-16 08:02:52 -0300

    criação do styles

---
+++ src/pages/ReduxPages/ReduxPage.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledReduxPageConteiner = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 1vh 5px 0 5px;
+  width: 100%;
+`;
