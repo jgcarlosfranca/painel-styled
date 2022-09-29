0ac154710c37786ed653b370fcb514b2a6eaae09
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-28 21:01:00 -0300

    movendo pasta do post styles

--- src/redux/features/posts/PostList.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledPostArticle = styled.article`
-  margin: 0.5em 0.5em 0.5em 0;
-  border: ${props => props.theme.colors.tables?.posts};
-  border-radius: 10px;
-  padding: 1em;
-`;
