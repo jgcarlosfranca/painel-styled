fc757ecd059b7a662e89ba83b82af38ed604ac95
joaogcarlosf <joaogcarlosf@gmail.com>
2022-09-30 15:29:26 -0300

    add novo component redux

--- src/pages/ReduxPages/ReduxPage.jsx
+++ src/pages/ReduxPages/ReduxPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { StyledReduxPageConteiner } from "./ReduxPage.styles";
 import { BackGroundFixedCard } from "../../components/BackGroundFixedCard/BackGroundFixedCard";
-import PostsList from "../../redux/features/posts/PostsList";
+import PostsList from "../../redux/features/posts/List/PostsList";
+import AddPostForm from "../../redux/features/posts/Add/AddPostForm";
 
 const ReduxPage = () => {
   return (
     <StyledReduxPageConteiner>
       <BackGroundFixedCard>
+        <AddPostForm />
         <PostsList />
       </BackGroundFixedCard>
     </StyledReduxPageConteiner>
