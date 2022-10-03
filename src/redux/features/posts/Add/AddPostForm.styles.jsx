46656a9efadaa7d317b2a2a85b3931ebb88f386e
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-03 10:22:19 -0300

    Criação de Styles para addPost

---
+++ src/redux/features/posts/Add/AddPostForm.styles.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const AddPostWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AddPostSection = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AddPostForms = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AddPostLabel = styled.label`
+  margin-top: 1em;
+  margin-bottom: 1.1em;
+`;
+export const AddPostinput = styled.input`
+  font: inherit;
+  margin-bottom: 1em;
+  border-color: #404656;
+  color: ${(props) => props.theme.colors.text};
+  background-color: ${(props) => props.theme.colors.contrastBackgroud};
+  &:active {
+    border-color: ${(props) => props.theme.colors.primaryButtonColor};
+  }
+  &:focus {
+    border-color: ${(props) => props.theme.colors.primaryButtonColor};
+  }
+`;
+
+export const AddPostContentTextArea = styled.textarea`
+  font: inherit;
+  margin-bottom: 1em;
+  border-color: #404656;
+  color: ${(props) => props.theme.colors.text};
+  background-color: ${(props) => props.theme.colors.contrastBackgroud};
+  &:active {
+    border-color: ${(props) => props.theme.colors.primaryButtonColor};
+  }
+  &:focus {
+    border-color: ${(props) => props.theme.colors.primaryButtonColor};
+  }
+`;
