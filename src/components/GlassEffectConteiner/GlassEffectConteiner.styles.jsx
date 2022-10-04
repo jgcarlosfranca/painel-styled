1b8afe367d8cea93deded8203812bb25057b3821
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-04 12:18:32 -0300

    fixing sticky glass conteiner

--- src/components/GlassEffectConteiner/GlassEffectConteiner.styles.jsx
+++ src/components/GlassEffectConteiner/GlassEffectConteiner.styles.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const StyledGlassEffectConteiner = styled.div`
+  z-index: 0;
   position: fixed;
   width: 100%;
-  top: 0%;
-  right: 0%;
+  top: 0;
+  right: 0;
   height: 5rem;
   background-color: ${(props) => props.theme.colors.glassBackGround};
   backdrop-filter: blur(2px);
