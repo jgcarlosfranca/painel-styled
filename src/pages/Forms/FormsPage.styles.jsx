acdb68ee100b5e5922bbdb43a668d10812d49e13
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-05 14:23:51 -0300

    fix Forms Conteiner

--- src/pages/Forms/FormsPage.styles.jsx
+++ src/pages/Forms/FormsPage.styles.jsx
@@ -4,6 +4,6 @@ export const StyledFormsPageConteiner = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  padding: 1vh 5px 0 5px;
+  padding: 3vh 5px 0 5px;
   width: 100%;
 `;
