326ba884ee8bc08f9190e67045719f2300b79e61
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-05 14:23:31 -0300

    fix glassConteiner

--- src/components/GlassEffectConteiner/GlassEffectConteiner.styles.jsx
+++ src/components/GlassEffectConteiner/GlassEffectConteiner.styles.jsx
@@ -6,7 +6,7 @@ export const StyledGlassEffectConteiner = styled.div`
   width: 100%;
   top: 0;
   right: 0;
-  height: 5rem;
+  height: 6.5rem;
   background-color: ${(props) => props.theme.colors.glassBackGround};
   backdrop-filter: blur(2px);
   -webkit-backdrop-filter: blur(2px);
