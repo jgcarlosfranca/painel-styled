3d57945685692cc4041ad7aec8347e63f5121935
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-12 12:36:13 -0300

    add userReducer no store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sideBarBehaviorReducer from "../features/sideBarBehaviorSlice";
 import postsReducer from "../features/posts/postsSlice";
+import userReducer from "../features/users/userSlice"
 
 
 export const store = configureStore({
     reducer: {
         sidebarBehavior: sideBarBehaviorReducer,
         posts: postsReducer,
+        users: userReducer,
     }
 })
