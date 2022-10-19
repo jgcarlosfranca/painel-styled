028b3828ef9060ba754c25b2ed6ee3c7e61bac47
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-19 18:44:13 -0300

    criando reaction button styles

---
+++ src/redux/features/posts/ReactionButton/ReactionButton.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledReactionButtons = styled.button`
+  margin: 0 0.25em 0 0;
+  background: transparent;
+  border: none;
+  color: whitesmoke;
+  font-size: 1rem;
+  cursor: pointer;
+`;
