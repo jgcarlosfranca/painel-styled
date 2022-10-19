7aad4b401cf033703d3e6d3031b88add1ea6eaf0
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-19 18:44:01 -0300

    criando reactionButton

--- src/redux/features/posts/ReactionButton/ReactionButton.jsx
+++ src/redux/features/posts/ReactionButton/ReactionButton.jsx
@@ -1,1 +1,33 @@
+import React from "react";
 import { useDispatch } from "react-redux";
+import { reactionAdded } from "../postsSlice";
+import { StyledReactionButtons } from "./ReactionButton.styles";
+
+const reactionEmoji = {
+  thumbsUp: "👍", // ctrl shift p> insert emoji
+  wow: "😲",
+  heart: "❤️",
+  rocket: "🚀",
+  coffee: "☕",
+};
+
+const ReactionButton = ({ post }) => {
+  const dispatch = useDispatch();
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return (
+      <StyledReactionButtons
+        key={name}
+        type="button"
+        onClick={() =>
+          dispatch(reactionAdded({ postId: post.id, reaction: name }))
+        }
+      >
+        {emoji}
+        {post.reactions[name]}
+      </StyledReactionButtons>
+    );
+  });
+  return <div>{reactionButtons}</div>;
+};
+
+export default ReactionButton;
