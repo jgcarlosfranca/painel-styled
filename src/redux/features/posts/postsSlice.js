a5c877a639c5da4cc24f4594478ccdc1206ce23e
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-19 18:20:53 -0300

    add reactions no PostSlice

--- src/redux/features/posts/postsSlice.js
+++ src/redux/features/posts/postsSlice.js
@@ -6,12 +6,26 @@ const initialState = [{
         title: 'Learning Redux Toolkit',
         content: "I've heard good things.",
         date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+        },
     },
     {
         id: '2',
         title: 'Slices...',
         content: "The more I say slice, the more I want pizza.",
         date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+        },
     }
 ]
 
@@ -32,6 +46,13 @@ const postsSlice = createSlice({
                         content,
                         date: new Date().toISOString(),
                         userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0,
+                        },
                     }
                 }
             }
