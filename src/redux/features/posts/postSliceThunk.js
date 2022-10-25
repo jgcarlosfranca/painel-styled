71cdfd7557c52e2e5ae230f6f7d0594f4d289fee
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-24 23:13:35 -0300

    mudando initialState para usar thunk

--- src/redux/features/posts/postSliceThunk.js
+++ src/redux/features/posts/postSliceThunk.js
@@ -2,32 +2,10 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns"
 
 const initialState = [{
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: "I've heard good things.",
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0,
-        },
-    },
-    {
-        id: '2',
-        title: 'Slices...',
-        content: "The more I say slice, the more I want pizza.",
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0,
-        },
-    }
-]
+    post: [],
+    status: 'idle', // 'idle', 'loading', 'succeeded', 'failed' 
+    error: null
+}]
 
 
 const postsSliceThunk = createSlice({
