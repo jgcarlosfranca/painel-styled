214ca562274a5cd5fb6b88a1412baa6d1d67fa07
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-26 16:08:42 -0300

    fiox initial state

--- src/redux/features/posts/postSliceThunk.js
+++ src/redux/features/posts/postSliceThunk.js
@@ -1,11 +1,11 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns"
 
-const initialState = [{
+const initialState = {
     post: [],
     status: 'idle', // 'idle', 'loading', 'succeeded', 'failed' 
     error: null
-}]
+}
 
 
 const postsSliceThunk = createSlice({
@@ -14,7 +14,7 @@ const postsSliceThunk = createSlice({
     reducers: {
         postAdded: {
             reducer(state, action) {
-                state.push(action.payload)
+                state.posts.push(action.payload)
             },
             prepare(title, content, userId) {
                 return {
@@ -37,7 +37,7 @@ const postsSliceThunk = createSlice({
         },
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload
-            const existingPost = state.find(post => post.id === postId)
+            const existingPost = state.posts.find(post => post.id === postId)
             if (existingPost) {
                 existingPost.reactions[reaction]++
             }
@@ -45,7 +45,7 @@ const postsSliceThunk = createSlice({
     }
 })
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 
 export const { postAdded, reactionAdded } = postsSliceThunk.actions;
 
