b807fa274d61524e5d4c9813ceb7c5ef7ff94dc6
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-27 20:38:31 -0300

    criando os fluxos de da api com redux thunk

--- src/redux/features/posts/postSliceThunk.js
+++ src/redux/features/posts/postSliceThunk.js
@@ -1,11 +1,19 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns"
+import axios from "axios";
+
+const POST_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const initialState = {
-    post: [],
+    posts: [],
     status: 'idle', // 'idle', 'loading', 'succeeded', 'failed' 
     error: null
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async() => {
+    const response = await axios.get(POST_URL)
+    return response.data
+})
 
 
 const postsSliceThunk = createSlice({
@@ -42,6 +50,35 @@ const postsSliceThunk = createSlice({
                 existingPost.reactions[reaction]++
             }
         }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                    // Adding date and reactions
+                let min = 1;
+                const loadedPosts = action.payload.map(post => {
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString();
+                    post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                    }
+                    return post;
+                });
+
+                // Add any fetched posts to the array
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
     }
 })
 
