ef54dce20ad36bd225d8f67272a9ccfc6e3e24e6
joaogcarlosf <joaogcarlosf@gmail.com>
2022-10-28 09:53:51 -0300

    fix color no reactionButton

--- src/redux/features/posts/ReactionButton/ReactionButton.styles.jsx
+++ src/redux/features/posts/ReactionButton/ReactionButton.styles.jsx
@@ -4,7 +4,7 @@ export const StyledReactionButtons = styled.button`
   margin: 0 0.25em 0 0;
   background: transparent;
   border: none;
-  color: whitesmoke;
+  color: ${(props) => props.theme.colors.text};
   font-size: 1rem;
   cursor: pointer;
 `;
