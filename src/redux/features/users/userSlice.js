1828f1968e04997d14013eeed1a3db96a77ab6ff
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-01 20:35:40 -0300

    criando fetchUsers com asyncThunk

--- src/redux/features/users/userSlice.js
+++ src/redux/features/users/userSlice.js
@@ -1,14 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [{ id: '0', name: 'Dude' },
-    { id: '1', name: 'Bob' },
-    { id: '0', name: 'John' },
-]
+const CUSTOM_POST_URL = 'http://localhost:5000/MyPostsList'; // precisa colocar o server up
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async() => {
+    const response = await axios.get(CUSTOM_POST_URL);
+    return response.data
+})
 
 const userSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload
+        })
+    }
 })
 
 export const selectAllUsers = (state) => state.users;
