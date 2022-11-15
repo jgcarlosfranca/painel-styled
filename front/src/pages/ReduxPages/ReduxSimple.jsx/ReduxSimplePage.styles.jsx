b598a683d84dbe039590e53f59c729e34f407c7c
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-15 17:58:45 -0300

    redux simple style

---
+++ front/src/pages/ReduxPages/ReduxSimple.jsx/ReduxSimplePage.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledReduxSimplePageConteiner = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 3vh 5px 0 5px;
+  width: 100%;
+`;
