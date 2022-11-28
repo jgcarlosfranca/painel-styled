90fb9499766ce07b47427e714512af51d0051b8f
joaogcarlosf <joaogcarlosf@gmail.com>
2022-11-28 20:57:20 -0300

    criação scrollToTheTop

---
+++ front/src/utils/useFullFunction/scrollToTheTop.js
@@ -0,0 +1,17 @@
+export const scrollToTheTop = () => {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+/**
+ * 
+scroll-behavior: auto;
+scroll-behavior: smooth;
+scroll-behavior: inherit;
+scroll-behavior: initial;
+scroll-behavior: revert;
+scroll-behavior: revert-layer;
+scroll-behavior: unset;
+ */
