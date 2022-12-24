7aff0d5d46b29345c4c1420587390b84a31f017a
joaogcarlosf <joaogcarlosf@gmail.com>
2022-12-24 16:50:55 -0300

    criando labFont

---
+++ front/src/pages/LabFont/LabFontPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { ConteinerFont } from "./LabFontPage.styles";
+
+const LabFontPage = () => {
+
+  return (
+    <ConteinerFont>
+    </ConteinerFont>
+  );
+};
+
+export default LabFontPage;
