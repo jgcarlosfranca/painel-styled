db6fd0d0210114f30348d61d613a1f15cb989ff2
joaogcarlosf <joaogcarlosf@gmail.com>
2023-01-18 15:38:40 -0300

    criando um lider de botões

--- front/src/pages/Beautify/Forms/BeautifyForms.jsx
+++ front/src/pages/Beautify/Forms/BeautifyForms.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  StyledBeautifyConteiner,
+} from "./BeatifyForms.styles";
 
 const BeautifyForms = () => {
   return (
-    <div>BautifyForms</div>
-  )
-}
+    <StyledBeautifyConteiner>
+      <div className="pager">
+        <button>
+          <span></span>
+        </button>
+        <button>
+          <span></span>
+        </button>
+        <button>
+          <span></span>
+        </button>
+        <button>
+          <span></span>
+        </button>
+        <button>
+          <span></span>
+        </button>
+      </div>
+    </StyledBeautifyConteiner>
+  );
+};
 
-export default BeautifyForms
+export default BeautifyForms;
