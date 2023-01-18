793db535efac1999a73fefafa06be2777fb6bba5
joaogcarlosf <joaogcarlosf@gmail.com>
2023-01-18 15:39:03 -0300

    styles do slider de botões

--- front/src/pages/Beautify/Forms/BeatifyForms.styles.jsx
+++ front/src/pages/Beautify/Forms/BeatifyForms.styles.jsx
@@ -5,3 +5,33 @@ export const LongLightButton = styled.button`
   background-color: transparent;
   cursor: pointer;
 `;
+
+export const StyledBeautifyConteiner = styled.div`
+
+  padding: 4vh 5px 0 5px;
+  .pager {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .pager button {
+    flex: 0 0 42px;
+    height: 24px;
+    padding: 6px;
+    transition: 0.4s;
+    border-radius: 24px;
+    color: "#383142";
+  }
+  .pager button span {
+    height: 30px;
+    width: 100%;
+    border-radius: 42px;
+    color: "#383142";
+    transition: 0.4s;
+  }
+
+  .pager button:focus {
+    flex: 0 0 80px;
+    color: "#8f44fd";
+  }
+`;
