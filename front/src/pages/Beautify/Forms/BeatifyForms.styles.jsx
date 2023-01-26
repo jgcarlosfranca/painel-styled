de230111ee081ed7aed6175bb2f71feb7587f785
joaogcarlosf <joaogcarlosf@gmail.com>
2023-01-26 09:40:01 -0300

    teste glummy

--- front/src/pages/Beautify/Forms/BeatifyForms.styles.jsx
+++ front/src/pages/Beautify/Forms/BeatifyForms.styles.jsx
@@ -35,4 +35,32 @@ export const StyledBeautifyConteiner = styled.div`
     flex: 0 0 80px;
     background-color: #8f44fd;
   }
+
+
+  .pager-glummy {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .pager-glummy button {
+    z-index: 99;
+    flex: 0 0 42px;
+    height: 24px;
+    padding: 6px;
+    transition: 0.4s;
+    border-radius: 24px;
+    background-color: #383142;
+  }
+  .pager-glummy button span {
+    height: 30px;
+    width: 100%;
+    border-radius: 42px;
+    background-color: #383142;
+    transition: 0.4s;
+  }
+
+  .pager-glummy button:focus {
+    flex: 0 0 80px;
+    background-color: #8f44fd;
+  }
 `;
