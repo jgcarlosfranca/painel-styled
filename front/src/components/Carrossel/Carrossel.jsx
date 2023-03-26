7e7f9a8504d6f710d38edd0d9ff4893ca55d6b90
joaogcarlosf <joaogcarlosf@gmail.com>
2023-03-25 21:38:03 -0300

    criando metodo de mover o carrossel

--- front/src/components/Carrossel/Carrossel.jsx
+++ front/src/components/Carrossel/Carrossel.jsx
@@ -1,28 +1,42 @@
-import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import {
   SlideWrapper,
   ImageBox,
   NavButton,
   ImageCaption,
 } from "./Carrossel.styles";
-import shinjukuJapan from "../../assets/img/shinjuku-japan-night-tokyo.jpg";
 import { BiRightArrow, BiLeftArrow } from "react-icons/bi";
 
 export function Carrossel(props) {
-  // const {
+  const [{ items, activeIndex }, setState] = useState({
+    items: props.items,
+    activeIndex: 0, // begin with the first item
+  });
 
-  // } = props;
+  const moveTo = (newIndex) => () => {
+    if (newIndex === -1) {
+      // jump from the first image to the last
+      setState((s) => ({ ...s, activeIndex: items.length - 1 }));
+      return;
+    }
+    if (newIndex === items.length) {
+      // jump from the last image to the first
+      setState((s) => ({ ...s, activeIndex: 0 }));
+      return;
+    }
+
+    setState((s) => ({ ...s, activeIndex: newIndex }));
+  };
 
   return (
     <SlideWrapper>
       <ImageBox>
-        <img alt="" src={shinjukuJapan} />
-        <ImageCaption>Shinjuku Japan</ImageCaption>
-        <NavButton position="left">
+        <img alt={items[activeIndex].caption} src={items[activeIndex].image} />
+        <ImageCaption>{items[activeIndex].caption}</ImageCaption>
+        <NavButton position="left" onClick={moveTo(activeIndex - 1)}>
           <BiLeftArrow />
         </NavButton>
-        <NavButton position="right">
+        <NavButton position="right" onClick={moveTo(activeIndex + 1)}>
           <BiRightArrow />
         </NavButton>
       </ImageBox>
