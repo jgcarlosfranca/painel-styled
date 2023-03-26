55d905f24ac846d13f06d72795009a9f9d365be2
joaogcarlosf <joaogcarlosf@gmail.com>
2023-03-25 21:37:24 -0300

    criando um payload para o carrossel

---
+++ front/src/mocks/CarrossselData/CarrosselItens.js
@@ -0,0 +1,19 @@
+import shinjukuJapan from "../../assets/img/shinjuku-japan-night-tokyo.jpg";
+import hongKong from "../../assets/img/Hong_Kong.jpg";
+import singapura from "../../assets/img/singapura.jpg";
+
+const slideItems = [{
+        image: shinjukuJapan,
+        caption: "Shinjuku Japan",
+    },
+    {
+        image: hongKong,
+        caption: "Hong Kong",
+    },
+    {
+        image: singapura,
+        caption: "Singapura",
+    },
+];
+
+export default slideItems;
