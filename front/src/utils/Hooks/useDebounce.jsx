aec8f8564fdee09bc6bc2204f73d1b49d0ff5362
joaogcarlosf <joaogcarlosf@gmail.com>
2023-03-30 20:30:22 -0300

    usage example

--- front/src/utils/Hooks/useDebounce.jsx
+++ front/src/utils/Hooks/useDebounce.jsx
@@ -14,3 +14,40 @@ export const useDebounce = (value, delay) => {
   }, [value, delay]);
   return debouncedValue;
 };
+
+
+/**
+ ********** USAGE 
+
+ import {useDebounce} 
+ import {useState, useEffect}
+
+ export const SearchBar = () => {
+ const debouncedSearchTerm = useDebounce(searchTerm, 500)
+ const [searchTerm, setSerachTerm] = useState("");
+ const [result, setResult] = useState([]);
+
+    // Function to fetch search results from API
+    async function fetchSearchResults() {
+        try{
+            const response = await fetch(`api/serach?q=${debouncedSearchTerm}`)
+            const data = await response.json()
+            setResults(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(()=>{
+        fetchSearchResults()
+    }, [debouncedSearchTerm])
+
+    return(
+        <>
+            <input type="text" value={searchTerm} onChage={(event) => setSearchTerm(event.target.value)} 
+            placeholder="Search..." />
+            <ul>{results.map((result)=>(<li key={crypto.randomUUID()}>{result.name}</li>))}</ul>
+        </>
+    )
+}
+ */
