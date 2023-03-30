5fb34b41a0731375e6e2ade3c11a4d41da93425b
joaogcarlosf <joaogcarlosf@gmail.com>
2023-03-30 20:16:42 -0300

    criação do hook de debounced

---
+++ front/src/utils/Hooks/useDebounce.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+  return debouncedValue;
+};
