4f7bbd3b86c24291fccd30259120548e18172a31
joaogcarlosf <joaogcarlosf@gmail.com>
2023-04-05 20:32:11 -0300

    criando component animado

--- front/src/components/Accordion/AccordionAnimated/Accordion.jsx
+++ front/src/components/Accordion/AccordionAnimated/Accordion.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
-
+import { AccordionWrapper, Title, Content, Text } from "./Accordion.styles";
 
 export const AccordionAnimated = (props) => {
-  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  return <></>;
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <AccordionWrapper>
+      <Title onClick={toggleAccordion}>{title}</Title>
+      <Content isOpen={isOpen}>
+        <Text isOpen={isOpen}>{content}</Text>
+      </Content>
+    </AccordionWrapper>
+  );
 };
