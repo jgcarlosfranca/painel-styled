9d2f792d6be022c209e356dca0c99c8f91f9a7d4
joaogcarlosf <joaogcarlosf@gmail.com>
2023-04-07 11:48:44 -0300

    criação de modal com slioder top down

--- front/src/components/Modal/Modal.jsx
+++ front/src/components/Modal/Modal.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import {
-  StyledModel,
-  StyledContainerModel,
-  StyledHeaderModal,
-  StyledHBarModal,
-  StyledSlotModal,
-  StyledButtonBarModal,
-  StyledButtonModal,
-} from "./Modal.styles";
+import { ModalWrapper, ModalContent } from "./Modal.styles";
 
-export default function ConfirmationModal(props) {
-
-}
+export const Modal = ({ isOpen, showing = false, onClose, children }) => {
+  return (
+    <ModalWrapper isOpen={isOpen} showing={showing} onClick={onClose}>
+      <ModalContent isOpen={isOpen} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </ModalContent>
+    </ModalWrapper>
+  );
+};
