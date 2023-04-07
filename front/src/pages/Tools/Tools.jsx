c945be03752dbfda96bfe450dc3c215ad580c2b4
joaogcarlosf <joaogcarlosf@gmail.com>
2023-04-07 11:49:23 -0300

    teste com a modal

--- front/src/pages/Tools/Tools.jsx
+++ front/src/pages/Tools/Tools.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledToolsConteiner } from "./Tools.styles";
 import Tabs from "../../components/Tab/Tab";
 import mocks from "../../mocks/mockDatas";
@@ -10,10 +10,13 @@ import imagemTeste from "../../assets/img/elemento_agua.png";
 import { Carrossel } from "../../components/Carrossel/Carrossel";
 
 import { AccordionAnimated } from "../../components/Accordion/AccordionAnimated";
+import { Modal } from "../../components/Modal/Modal";
 
 import slideItems from "../../mocks/CarrossselData/CarrosselItens";
 
 const Tools = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <StyledToolsConteiner>
       <Tabs tabs={mocks.numTabs} />
@@ -39,6 +42,16 @@ const Tools = () => {
       <Card>
         <CardButton image={imagemTeste} onClick={() => console.log("clicou")} />
         <Button />
+      </Card>
+      <Card>
+        <button onClick={() => setIsOpen(!isOpen)}>teste</button>
+        <Modal
+          isOpen={isOpen}
+          showing={isOpen}
+          onClose={() => setIsOpen(!isOpen)}
+        >
+          teste com modal
+        </Modal>
       </Card>
       <Card>
         <Carrossel items={slideItems} />
