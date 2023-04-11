1d51cbf5fa6aeac6a24ee820484375406af342ad
joaogcarlosf <joaogcarlosf@gmail.com>
2023-04-11 10:17:12 -0300

    reajust spinner to use theme

--- front/src/components/Spinner/Spinner.styles.jsx
+++ front/src/components/Spinner/Spinner.styles.jsx
@@ -13,7 +13,8 @@ export const Spinner = styled.div`
   display: inline-block;
   width: ${(props) => props.size}px;
   height: ${(props) => props.size}px;
-  border: ${(props) => props.size / 10}px solid ${(props) => props.color};
+  border: ${(props) => props.size / 10}px solid
+    ${(props) => (props.color ? props.color : props.theme.colors.loadingSpiner)};
   border-top-color: transparent;
   border-radius: 50%;
   animation: ${spin} 0.8s linear infinite;
