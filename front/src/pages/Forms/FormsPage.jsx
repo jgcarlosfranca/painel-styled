d4c3cdf1bf3174cff32248ddb0b1f1d5f2659f0f
joaogcarlosf <joaogcarlosf@gmail.com>
2023-04-12 16:40:08 -0300

    testando o multiselect na pagina de forms

--- front/src/pages/Forms/FormsPage.jsx
+++ front/src/pages/Forms/FormsPage.jsx
@@ -3,9 +3,17 @@ import { StyledFormsPageConteiner } from "./FormsPage.styles";
 import { BackGroundFixedCard } from "../../components/BackGroundFixedCard/BackGroundFixedCard";
 import ToggleSwitchCustom from "../../components/SwitchToogle/switchCustom";
 
+import { Card } from "../../components/Card";
+import MultiSelect from "../../components/Forms/MultiSelect/MultiSelect";
+
 const FormsPage = () => {
   const [switchToogle, setSwicthToogle] = useState(true);
 
+  const options = [
+    { value: "option1", label: "Option 1" },
+    { value: "option2", label: "Option 2" },
+    { value: "option3", label: "Option 3" },
+  ];
   return (
     <StyledFormsPageConteiner>
       <BackGroundFixedCard>
@@ -16,6 +24,9 @@ const FormsPage = () => {
           handleChangeCheckBox={() => setSwicthToogle(!switchToogle)}
         />
       </BackGroundFixedCard>
+      <Card>
+        <MultiSelect options={options} />
+      </Card>
     </StyledFormsPageConteiner>
   );
 };
