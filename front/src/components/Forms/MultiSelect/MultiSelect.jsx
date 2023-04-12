4b0c96a792b7492aaf290a6a1d6cb85a93265ca8
joaogcarlosf <joaogcarlosf@gmail.com>
2023-04-12 16:39:13 -0300

    criando component de multselect

---
+++ front/src/components/Forms/MultiSelect/MultiSelect.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+  MultiSelectWrapper,
+  MultiSelectButton,
+  MultiSelectList,
+  MultiSelectItem,
+} from "./MultiSelect.styles";
+
+const MultiSelect = ({ options }) => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => setIsOpen(!isOpen);
+
+  const handleOptionClick = (optionValue) => {
+    const isOptionSelected = selectedOptions.includes(optionValue);
+
+    if (isOptionSelected) {
+      setSelectedOptions(
+        selectedOptions.filter((value) => value !== optionValue)
+      );
+    } else {
+      setSelectedOptions([...selectedOptions, optionValue]);
+    }
+  };
+
+  return (
+    <MultiSelectWrapper>
+      <MultiSelectButton onClick={handleToggle}>
+        {selectedOptions.length === 0
+          ? "Select options"
+          : selectedOptions.join(", ")}
+      </MultiSelectButton>
+      {isOpen && (
+        <MultiSelectList>
+          {options.map((option) => (
+            <MultiSelectItem
+              key={option.value}
+              isSelected={selectedOptions.includes(option.value)}
+              onClick={() => handleOptionClick(option.value)}
+            >
+              {option.label}
+            </MultiSelectItem>
+          ))}
+        </MultiSelectList>
+      )}
+    </MultiSelectWrapper>
+  );
+};
+
+export default MultiSelect;
