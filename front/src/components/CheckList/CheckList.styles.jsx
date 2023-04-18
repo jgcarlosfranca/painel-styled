46f18c02a39a819d03534529300eecc2a53a13f1
joaogcarlosf <joaogcarlosf@gmail.com>
2023-04-17 21:49:44 -0300

    Criando CheckList styles

---
+++ front/src/components/CheckList/CheckList.styles.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const Item = styled.label`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const Checkbox = styled.input`
+  margin-right: 10px;
+`;
